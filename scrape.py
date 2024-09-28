c0e9328a27e433edcdcabdd815617362c02b1d5d
Pavan Kumar V <pkvstarscream@gmail.com>
2024-09-28 14:39:12 +0530

    add chromedriver

---
+++ scrape.py
@@ -0,0 +1,19 @@
+import selenium.webdriver as webdriver
+from selenium.webdriver.chrome.service import Service
+import time
+
+def scrape_website(website):
+    print("Launching chrome driver...")
+
+    chrome_driver_path = ""
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=Service(chrome_driver_path), options=options)
+
+    try:
+        driver.get(website)
+        print("Page loaded...")
+        html = driver.page_source
+        time.sleep(10)
+        return html
+    finally:
+        driver.quit()
