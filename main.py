7a90c2b777b0082c3eca69ea1589746040af81c2
Pavan Kumar V <pkvstarscream@gmail.com>
2024-09-28 15:40:33 +0530

    integrate scraping functions with UI in main.py

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 # Step 1: Create a very simple streamlit user interface
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the website")
+    
     result = scrape_website(url)
-    print(result)
-# Step 2: Grab data from the website that we want to scrape
+    body_content = extract_body_content(result)
+    clean_content = clean_body_content(body_content)
+
+    st.session_state.dom_content = clean_content
+    # Expander textbook - allows us to expland it to view more content
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", clean_content, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
--- scrape.py
+++ scrape.py
@@ -1,8 +1,8 @@
 import time
 import selenium.webdriver as webdriver
 from selenium.webdriver.chrome.service import Service
-
-
+from bs4 import BeautifulSoup
+# Step 2: Grab data from the website that we want to scrape
 def scrape_website(website):
     print("Launching chrome driver...")
 
@@ -18,3 +18,32 @@ def scrape_website(website):
         return html
     finally:
         driver1.quit()
+
+
+# Extracting only body content from the scraped website
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+# Clean the body content(no need of script and style tage)
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+    # get all of the text and then separate it with a new line
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+        line.strip() for line in cleaned_content.splitlines() if line.strip()
+    )
+
+    return cleaned_content
+
+# Split the content due to the token contraint in the LLM
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i: i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
